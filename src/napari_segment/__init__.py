2546437082e1da5415b70bb27d1c34c54c765ffe
Andrey Aristov <aaristov@pasteur.fr>
2022-08-31 17:15:50 +0200

    add __all__

--- src/napari_segment/__init__.py
+++ src/napari_segment/__init__.py
@@ -1,4 +1,3 @@
-
 try:
     from ._version import version as __version__
 except ImportError:
@@ -6,6 +5,15 @@ except ImportError:
 
 
 from ._reader import napari_get_reader
-from ._writer import write_single_image, write_multiple 
-from ._sample_data import make_sample_data 
+from ._sample_data import make_sample_data
 from ._widget import ExampleQWidget, example_magic_widget
+from ._writer import write_multiple, write_single_image
+
+__ALL__ = [
+    napari_get_reader,
+    write_multiple,
+    write_single_image,
+    make_sample_data,
+    ExampleQWidget,
+    example_magic_widget,
+]
