5fcc871364beda89c287988a9a41efade26d9fbd
Andrey Aristov <aaristov@pasteur.fr>
2022-11-15 16:33:11 +0100

    Improve tests:
    - test with sample data,
    - plot data,
    - make manual layer

--- src/napari_segment/_tests/test_widget.py
+++ src/napari_segment/_tests/test_widget.py
@@ -1,22 +1,34 @@
-from napari_segment import SegmentStack
-import numpy as np
+import os
+
 import dask.array as da
+import numpy as np
+
+from napari_segment import SegmentStack
+
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_segment_stack(make_napari_viewer, capsys):
+def test_segment_stack(make_napari_viewer):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((5, 100, 100)), name="test_data", metadata={"path": "fakepath.nd2"})
+    viewer.open_sample("napari-segment", "D3_D4")
 
     # create our widget, passing in the viewer
     widget = SegmentStack(viewer)
-    assert widget.input.current_choice == "test_data"
-    assert widget.path == "fakepath.nd2"
-    assert isinstance(widget.ddata, da.Array) 
-    assert widget.ddata.chunks == ((1, 1, 1, 1, 1), (100/widget.binning,), (100/widget.binning,))
+    assert widget.input.current_choice == "D3_D4"
+    assert ".napari-segment/data/D3_D4.nd2" in widget.path
+    assert isinstance(widget.ddata, da.Array)
 
-    
+    widget.save_params()
+    assert os.path.exists(widget.path + ".params.yaml")
+
+    widget.plot_stats(True)
+
+    widget.make_manual_layer()
+
+    assert (mtitle := "Manual Labels") in viewer.layers
+    assert isinstance(viewer.layers[mtitle].data, np.ndarray)
+
     # read captured output and check that it's as we expected
     # captured = capsys.readouterr()
     # assert captured.out == "napari has 1 layers\n"
